219015b68ad6abb16f7e54ee693e67648fe1e0ba
André Castillo <89285611+andrecasmac@users.noreply.github.com>
2022-11-15 18:28:28 -0700

    Added Arduino file

---
+++ gyroscopeReadings.ino
@@ -0,0 +1,38 @@
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+Adafruit_MPU6050 mpu;
+
+void setup(void) {
+  Serial.begin(115200);
+  while (!Serial)
+    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+
+  Serial.println("Adafruit MPU6050 test!");
+
+  mpu.begin();
+  Serial.println("MPU6050 Found!");
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_260_HZ);
+
+  delay(2000);
+}
+
+void loop() {
+  /* Get new sensor events with the readings */
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  /* Print out the values */
+  Serial.print("X:");
+  Serial.print(a.acceleration.x);
+  Serial.print(",Y:");
+  Serial.print(a.acceleration.y);
+  Serial.print(",Z:");
+  Serial.println(a.acceleration.z);
+
+  delay(2000);
+}
